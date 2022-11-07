44be355f180d895c26f61838a6cd7fa27443ebc7
Sus <oliversharp2005@gmail.com>
2022-11-07 20:42:48 +0000

    release pain
    removed /pages/ :)
    
    html docs in root = fun

--- components/header.js
+++ components/header.js
@@ -7,14 +7,14 @@ class Header extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-dark" id="navbar">
           <div class="container-fluid">
-            <a class="navbar-brand" href="#">Oliver's Blog</a>
+            <a class="navbar-brand" href="./index.html">Oliver's Blog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="../index.html">Home</a>
+                  <a class="nav-link" aria-current="page" href="./index.html">Home</a>
                 </li>
                 <!-- -->
                 <li class="nav-item dropdown">
@@ -22,9 +22,9 @@ class Header extends HTMLElement {
                     Programming
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="../pages/c.html">C</a></li>
-                    <li><a class="dropdown-item" href="../pages/python.html">Python</a></li>
-                    <li><a class="dropdown-item" href="../pages/html+css.html">HTML/CSS</a></li>
+                    <li><a class="dropdown-item" href="./c.html">C</a></li>
+                    <li><a class="dropdown-item" href="./python.html">Python</a></li>
+                    <li><a class="dropdown-item" href="./html+css.html">HTML/CSS</a></li>
                   </ul>
                 </li>
                 <!-- -->
@@ -33,8 +33,8 @@ class Header extends HTMLElement {
                     Content Areas
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="../pages/ca 1-4.html">CA 1-4</a></li>
-                    <li><a class="dropdown-item" href="../pages/ca 5-8.html">CA 5-8</a></li>
+                    <li><a class="dropdown-item" href="./ca 1-4.html">CA 1-4</a></li>
+                    <li><a class="dropdown-item" href="./ca 5-8.html">CA 5-8</a></li>
                   </ul>
                 </li>
                 <!-- -->
@@ -43,24 +43,24 @@ class Header extends HTMLElement {
                     Projects
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="../pages/ui+ux.html">UI/UX</a></li>
+                    <li><a class="dropdown-item" href="./ui+ux.html">UI/UX</a></li>
                   </ul>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="../pages/industry placement.html">Industry Placement</a>
+                  <a class="nav-link" aria-current="page" href="./industry placement.html">Industry Placement</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="../pages/events.html">Events</a>
+                  <a class="nav-link" aria-current="page" href="./events.html">Events</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="../pages/blog.html">Blog</a>
+                  <a class="nav-link" aria-current="page" href="./blog.html">Blog</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="../pages/masterclasses.html">Masterclasses</a>
+                  <a class="nav-link" aria-current="page" href="./masterclasses.html">Masterclasses</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
