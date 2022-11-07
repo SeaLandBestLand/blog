8f0edb5bf1f02cf068175f00376ec1cc91b24524
Sus <oliversharp2005@gmail.com>
2022-11-07 12:10:00 +0000

    added navigation to other pages

--- components/header.js
+++ components/header.js
@@ -22,9 +22,9 @@ class Header extends HTMLElement {
                     Programming
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="#">C</a></li>
-                    <li><a class="dropdown-item" href="#">Python</a></li>
-                    <li><a class="dropdown-item" href="#">HTML/CSS</a></li>
+                    <li><a class="dropdown-item" href="../pages/c.html">C</a></li>
+                    <li><a class="dropdown-item" href="../pages/python.html">Python</a></li>
+                    <li><a class="dropdown-item" href="../pages/html+css.html">HTML/CSS</a></li>
                   </ul>
                 </li>
                 <!-- -->
@@ -33,8 +33,8 @@ class Header extends HTMLElement {
                     Content Areas
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="#">CA 1-4</a></li>
-                    <li><a class="dropdown-item" href="#">CA 5-8</a></li>
+                    <li><a class="dropdown-item" href="../pages/ca 1-4.html">CA 1-4</a></li>
+                    <li><a class="dropdown-item" href="../pages/ca 5-8.html">CA 5-8</a></li>
                   </ul>
                 </li>
                 <!-- -->
@@ -43,24 +43,24 @@ class Header extends HTMLElement {
                     Projects
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="#">UI/UX</a></li>
+                    <li><a class="dropdown-item" href="../pages/ui+ux.html">UI/UX</a></li>
                   </ul>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Industry Placement</a>
+                  <a class="nav-link" aria-current="page" href="../pages/industry placement.html">Industry Placement</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Events</a>
+                  <a class="nav-link" aria-current="page" href="../pages/events.html">Events</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Blog</a>
+                  <a class="nav-link" aria-current="page" href="../pages/blog.html">Blog</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
-                  <a class="nav-link" aria-current="page" href="#">Masterclasses</a>
+                  <a class="nav-link" aria-current="page" href="../pages/masterclasses.html">Masterclasses</a>
                 </li>
                 <!-- -->
                 <li class="nav-item">
